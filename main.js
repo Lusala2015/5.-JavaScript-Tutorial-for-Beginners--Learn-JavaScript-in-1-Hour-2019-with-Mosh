2e017583270d63433ea4a8819d4ce1f99dde5088
Lusala2015 <dev.mmlusala@gmail.com>
2020-08-05 14:18:46 +0300

    end of functions

--- main.js
+++ main.js
@@ -37,9 +37,20 @@ selectorColors[2] = 'han';
 console.log(selectorColors.length);
 
 //FUNCTIONS 
+//performs a task
 function greet(name){
     console.log('Hello ' + name);
 }
 
 greet('Lusala');
 greet('Faith');
+
+
+//types of functions 
+//Calculating a value 
+function square(number){
+    return number * number;
+}
+let number = square(9);
+console.log(number)
+console.log(square(6));
